86dd4ed77137a6a63036b17c38d8525cb715da26
Iulia <iuly_alex2000@yahoo.com>
2025-07-07 15:46:54 +0300

    Replaced whisper with whisperx to extract word timestamps.

--- STT/main.py
+++ STT/main.py
@@ -1,10 +1,24 @@
 from data_loader import DataLoader
-import whisper
+import whisperx
+import torch
 
-data_loader = DataLoader("../video resources/v4.mp4")
+video_path = "../video resources/v1.mp4"
+data_loader = DataLoader(video_path)
 data = data_loader.load_data()
 
-model = whisper.load_model("large-v3")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model = whisperx.load_model("large-v3", device=device, compute_type="int8")
 result = model.transcribe(data.get("audio_path"), language="ro")
-print(result["text"])
+print(result)
 print("Detected language:", result.get("language", "unknown"))
+
+model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+aligned_result = whisperx.align(result["segments"], model_a, metadata, video_path, device="cpu")
+
+print("Aligned segments:", aligned_result["word_segments"])
+
+for word in aligned_result["word_segments"]:
+    if all(k in word for k in ("text", "start", "end")):
+        print(f"{word['text']} => {word['start']} - {word['end']}")
+    else:
+        print("Missing or incomplete segment:", word)
